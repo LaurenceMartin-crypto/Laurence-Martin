faeb522285bf1ab6212fc1ef384ee1ee0b2bddf2
jpowell3617 <jpowell3617@g.fmarion.edu>
2018-01-31 12:16:16 -0500

    Enhance: Add routing code for join-screen component.

--- Project-Dynamo.Front/src/app/app.component.ts
+++ Project-Dynamo.Front/src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+//import { JoinHostScreenComponent } from './join-host-screen/join-host-screen.component';
+import { JoinScreenComponent } from './join-screen/join-screen.component';
+
+
 
 @Component({
   selector: 'app-root',
--- Project-Dynamo.Front/src/app/app.module.ts
+++ Project-Dynamo.Front/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { JoinHostScreenComponent } from './join-host-screen/join-host-screen.component';
 import { JoinScreenComponent } from './join-screen/join-screen.component';
+
+const routes: Routes = [
+  { path: '', component: JoinHostScreenComponent }
+  { path: '', component: JoinScreenComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    JoinHostScreenComponent,
     JoinScreenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
